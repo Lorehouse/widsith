bbe7ed2697ac23800388252e8d2d56c662a7292d
Sarah Monnier <sarahjmonnier@gmail.com>
2024-10-18 13:10:17 -0500

    refined the tsvtohtml code

--- sandbox/tsvtohtml.py
+++ sandbox/tsvtohtml.py
@@ -7,11 +7,14 @@ def tsv_to_html(tsv_file, html_file):
     
     
     with open(html_file, 'w', encoding='utf-8') as file:
-        file.write('<html>\n<head>\n<title>Glossary</title>\n</head>\n<body>\n')
+        file.write('<html>\n<head>\n\t<meta charset="UTF-8">\n\t<title>Glossary</title>\n\t<link rel="stylesheet" href="glossstyles.css">\n</head>\n<body>\n')
+        file.write('<button class="button" onclick="document.location=\'https://lorehord.com/\'">Home</button>')
+        file.write('<h1>Glossary</h1>')
         file.write('<table border="1">\n')
-
+        for column in data[0]:
+            file.write(f'    <th>{column}</th>\n')
         
-        for row in data:
+        for row in data[1:]:
             file.write('  <tr>\n')
             for column in row:
                 file.write(f'    <td>{column}</td>\n')
@@ -19,6 +22,7 @@ def tsv_to_html(tsv_file, html_file):
         
         
         file.write('</table>\n</body>\n</html>')
+    print(f"Your file has been created at '{html_file}'.")
 
 
 tsv_to_html('../gloss.tsv', '../views/glossary.html')
